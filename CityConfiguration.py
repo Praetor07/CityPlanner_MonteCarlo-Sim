30da5b2048a85e96c765902139662f25612e6275
Shruti Ravichandran <shrutiravi04@gmail.com>
2022-11-28 19:50:38 -0600

    Added code missed in merge

--- CityConfiguration.py
+++ CityConfiguration.py
@@ -1,8 +1,39 @@
 import numpy as np
 import networkx as nx
 
+
+# Function for mod_pert_random has been picked from Mr Weible's example
+# from https://github.com/iSchool-597PR/2022_Fall_examples/blob/main/unit_07/Probability_Distributions.ipynb
+def mod_pert_random(low, likely, high, confidence=4, samples=1):
+    """Produce random numbers according to the 'Modified PERT' distribution.
+    :param low: The lowest value expected as possible.
+    :param likely: The 'most likely' value, statistically, the mode.
+    :param high: The highest value expected as possible.
+    :param confidence: This is typically called 'lambda' in literature
+                        about the Modified PERT distribution. The value
+                        4 here matches the standard PERT curve. Higher
+                        values indicate higher confidence in the mode.
+                        Currently allows values 1-18
+    Formulas to convert beta to PERT are adapted from a whitepaper
+    "Modified Pert Simulation" by Paulo Buchsbaum.
+    """
+    # Check for reasonable confidence levels to allow:
+    if confidence < 1 or confidence > 18:
+        raise ValueError('confidence value must be in range 1-18.')
+
+    mean = (low + confidence * likely + high) / (confidence + 2)
+
+    a = (mean - low) / (high - low) * (confidence + 2)
+    b = ((confidence + 1) * high - low - confidence * likely) / (high - low)
+
+    beta = np.random.beta(a, b, samples)
+    beta = beta * (high - low) + low
+    return beta
+
+
 class City:
     zone_dimension = 3
+
     # Class variable - represents size of each zone in terms of coordinates
     # Class variable - represents constant time between two adjacent nodes with no traffic between them
     def __init__(self, width: int, height: int, zone_populations: list, intensity_distribution: list):
@@ -15,7 +46,6 @@ class City:
         (this will be used to randomize zone of an emergency as a distribution of the population
         density and then determine a random coordinate within the zone)
         """
-        self.city_graph = nx.DiGraph()
         self.width = width
         self.height = height
         self.zone_populations = zone_populations
@@ -23,9 +53,31 @@ class City:
         self.intensity_distribution = intensity_distribution
         self.intensity_cumulative = np.cumsum(np.rint(np.asarray(self.intensity_distribution)))
         for i in range(len(zone_populations)):
-            for _ in range(City.zone_dimension ** 2):# number of nodes per zone?
-                self.coordinate_populations.append(zone_populations[i]/(City.zone_dimension ** 2))
+            for _ in range(City.zone_dimension ** 2):  # number of nodes per zone?
+                self.coordinate_populations.append(zone_populations[i] / (City.zone_dimension ** 2))
+
+        self.city_graph = nx.DiGraph()
         self.build_city_graph()
+        # 0,0   0,1   0,2   0,3   0,4   0,5
+        # 1,0   1,1   1,2   1,3   1,4   1,5
+        # 2,0   2,1   2,2   2,3   2,4   2,5
+        # 3,0   3,1   3,2   3,3   3,4   3,5
+        # 4,0   4,1   4,2   4,3   4,4   4,5
+        # 5,0   5,1   5,2   5,3   5,4   5,5
+
+    def get_commute_time(self, time_of_day: int, zone_pop: int, city_pop: int):
+        """
+        Calculates the commute time based on time of day and the zone population
+        :param time_of_day: Time of day during the simulation
+        :param zone_pop: Average population of the zones involved in the commute
+        :param city_pop:
+        :return:
+        """
+        likely_val = zone_pop / city_pop
+        traffic_time = mod_pert_random(low=0, likely=likely_val, high=1, samples=1)
+        traffic_time *= time_of_day
+        commute_time = 10 + traffic_time
+        return commute_time
 
     def build_city_graph(self):
         # Modeling each zone using 9 nodes as a 3 x 3 set of nodes
@@ -39,7 +91,8 @@ class City:
                 # print(zone_num)
                 self.city_graph.add_nodes_from([((i, j), {'Zone_Number': zone_num,
                                                           'Zone_Population': self.zone_populations[zone_num],
-                                                          'Coord_Population': self.zone_populations[zone_num] / City.zone_dimension**2})])
+                                                          'Coord_Population': self.zone_populations[
+                                                                                  zone_num] / City.zone_dimension ** 2})])
             if (i == 0) or (i % City.zone_dimension != 0):
                 zone_num -= self.width
 
@@ -51,7 +104,6 @@ class City:
             if self.city_graph.has_node((i + 1, j)):
                 self.city_graph.add_edge((i, j), (i + 1, j), adjusted_time=10)
 
-
     def update_graph_edges(self, time_of_day: int, zone_pop: int, city_pop: int):
         """
         Update graph edges which is constant + traffic parameter
@@ -61,7 +113,6 @@ class City:
         for (i, j) in self.city_graph.nodes:
             travel_time = self.get_commute_time(time_of_day, zone_pop, city_pop)
             if self.city_graph.has_node((i, j + 1)):
-                self.city_graph[(i, j)][(i, j+1)]['adjusted_time'] = travel_time
+                self.city_graph[(i, j)][(i, j + 1)]['adjusted_time'] = travel_time
             if self.city_graph.has_node((i + 1, j)):
                 self.city_graph[(i, j)][(i + 1, j)]['adjusted_time'] = travel_time
-
